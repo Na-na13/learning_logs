a5a51741c26db661d801811393e97e779477c5f2
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2024-03-30 15:26:36 +0200

    View for individual topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # Home Page
     path("", views.index, name="index"),
     # Show all topics
-    path("topics", views.topics, name='topics')
+    path("topics", views.topics, name='topics'),
+    # Detail page for a single topic
+    path("topics/<int:topic_id>", views.topic, name='topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,8 +8,17 @@ def index(request):
     """The home page for LearningLog"""
     return render(request, 'learning_logs/index.html')
 
+
 def topics(request):
     """Show all topics"""
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.all()
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
