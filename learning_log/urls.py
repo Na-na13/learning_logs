4178bbf333495c399e9532f133a62ce89f8f3f46
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2024-04-01 17:12:33 +0300

    User login

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs')),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
 ]
---
+++ users/urls.py
@@ -0,0 +1,10 @@
+"""Defines URL patterns for users"""
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+urlpatterns = [
+    # Login page
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
+]
