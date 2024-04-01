3088fd512e15ee950bcc38c6c4c8c63961bcb7d1
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2024-04-01 19:38:09 +0300

    User logout

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     # Login page
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
+    path('login', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', views.logout_view, name='logout'),
 ]
